ada2e2b2d1a37ac10f8c249fd382c0df64275400
KimDaeYong123 <gimdaeyong@DYui-MacBook-Pro.local>
2015-10-04 13:35:01 +0900

    add
    add

---
+++ Assets/3_Script/ad.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using GoogleMobileAds.Api;
+
+public class AdmobManager : MonoBehaviour {
+	
+	void Start () {
+		BannerView banner = new BannerView("ca-app-pub-6153759682048197/6620940862", AdSize.Banner, AdPosition.Bottom);
+		
+		AdRequest request = new AdRequest.Builder().Build();
+		
+		banner.LoadAd(request);
+		banner.Show();
+	}
+}
